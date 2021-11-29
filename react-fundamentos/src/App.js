5bf725298dd5fbd1dd1708911fe94c6f3c103c22
Enrico B. Palma <enricob.palma@gmail.com>
2021-11-29 09:30:52 -0300

    States

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,35 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 import Post from './Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#03', likes: 50 },
+  ]);
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+          ...prevState,
+          {
+            id: Math.random(),
+            title: `Title#0${prevState.length + 1}`,
+            subtitle: `Sub#0${prevState.length + 1}`,
+            likes: 50
+          },
+      ]);
+    }, 2000);
+  }
+
   return (
     <>
-      <Header title="Bananinha Blog">
-        <h2>Posts da Semana</h2>
+      <Header>
+        <h2>
+          Posts da Semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
 
-      <Post
-        post={{
-          title: 'Título da Notícia 01',
-          subtitle: 'Subtítulo da Notícia 01',
-        }}
-      />
-      <Post
-        post={{
-          title: 'Título da Notícia 02',
-          subtitle: 'Subtítulo da Notícia 02',
-        }}
-      />
-      <Post
-        post={{
-          title: 'Título da Notícia 03',
-          subtitle: 'Subtítulo da Notícia 03',
-        }}
-      />
+      {posts.map(post => (
+        <Post
+          key={post.id}
+          likes={post.likes}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle,
+          }}
+        />
+      ))}
     </>
   );
 }

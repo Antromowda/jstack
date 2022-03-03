df83cffbfde8c4496da235c0fa1c6e44635a5e0e
Enrico B. Palma <enricob.palma@gmail.com>
2022-03-03 18:18:50 -0300

    feat: contacts cards

--- mycontacts/front_end/src/assets/styles/global.js
+++ mycontacts/front_end/src/assets/styles/global.js
@@ -10,7 +10,8 @@ export default createGlobalStyle`
 
   body {
     background: ${({ theme }) => theme.colors.background};
-    font-size: 1rem;
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.gray[900]}
   }
 
   button {
--- mycontacts/front_end/src/components/ContactsList/index.js
+++ mycontacts/front_end/src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +13,76 @@ export default function ContactsList() {
         <strong>3 Contatos</strong>
         <a href="/">Novo Contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="Setinha para ordenação." />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Enrico Palma</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>enrico@dev.gg</span>
+            <span>(14) 91234-5678</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Botão de Edição de Contato" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Botão para Remoção de Contato" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Enrico Palma</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>enrico@dev.gg</span>
+            <span>(14) 91234-5678</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Botão de Edição de Contato" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Botão para Remoção de Contato" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Enrico Palma</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>enrico@dev.gg</span>
+            <span>(14) 91234-5678</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Botão de Edição de Contato" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Botão para Remoção de Contato" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
